c19eb5cfd516426f9205ce15f5e05a5e7550217b
shanejearley <shane.earley10@gmail.com>
2021-09-05 18:46:52 -0400

    Add checkmark to radio

--- theme-app-extension/assets/tip-section.js
+++ theme-app-extension/assets/tip-section.js
@@ -4,25 +4,21 @@ class TipSection extends HTMLElement {
   }
 
   connectedCallback() {
-    console.log('Running connected callback.')
-    if(!this.radioChecked) this.radioChecked = {};
-    this.querySelectorAll("input[type='radio']").forEach(radio => {
-      radio.setAttribute("onclick", this.radioClick);
-      radio.setAttribute("onkeyup", this.radioClick);
+    document.radioChecked = {};
+    document.radioClick = function(e) {
+      const obj = e.target, name = obj.name || "unnamed";
+      if (e.keyCode) return obj.checked = e.keyCode !== 32;
+      obj.checked = document.radioChecked[name] !== obj;
+      document.radioChecked[name] = obj.checked ? obj : null;
+    }
+    document.querySelectorAll("input[type='radio']").forEach( radio => {
+      radio.setAttribute("onclick", "radioClick(event)");
+      radio.setAttribute("onkeyup", "radioClick(event)");
     });
   }
 
   disconnectedCallback() {
     console.log('Running disconnected callback.')
-  }
-
-  radioClick(event) {
-    console.log('Running radio click.')
-    const obj = event.target;
-    const name = obj.name || "unnamed";
-    if(event.keyCode) return obj.checked = event.keyCode!=32;
-    obj.checked = this.radioChecked[name] != obj;
-    this.radioChecked[name] = obj.checked ? obj : null;
   }
 }
 
