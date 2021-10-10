b9df09d9941ecf11f2cf71efdfe0bc2cb2e121c9
shanejearley <shane.earley10@gmail.com>
2021-10-10 18:07:44 -0400

    Add images to public folder

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -7,8 +7,8 @@ import {
 } from "@shopify/polaris";
 import styles from "./welcome.module.css";
 
-const OnboardingSrc = require(`${process.env.NODE_ENV !== 'production' ? '' : 'static'}/images/Onboarding.svg`);
-const HeyThanksSrc = require(`${process.env.NODE_ENV !== 'production' ? '' : 'static'}/images/HeyThanks.svg`);
+const OnboardingSrc = require(`${process.env.NODE_ENV !== 'production' ? '/public' : ''}/images/Onboarding.svg`);
+const HeyThanksSrc = require(`${process.env.NODE_ENV !== 'production' ? '/public' : ''}/images/HeyThanks.svg`);
 
 const Welcome = (props) => {
   const { name, currentStep, setCurrentStep } = props;
